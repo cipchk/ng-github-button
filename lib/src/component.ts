fc491d5dab5db90b166d24b12495e5a46893aa49
卡色 <cipchk@qq.com>
2022-06-24 17:24:26 +0800

    release: 14.0.0 (#7)

--- lib/src/component.ts
+++ lib/src/component.ts
@@ -10,17 +10,18 @@ import {
 } from '@angular/core';
 import { GithubButtonService } from './service';
 import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 const isSSR = !(typeof document === 'object' && !!document);
 
 @Component({
   selector: 'github-button',
   template: `
-    <a class="gh-btn" href="{{ repo_url }}" target="_blank">
+    <a class="gh-btn" [href]="repo_url" target="_blank">
       <span class="gh-ico" aria-hidden="true"></span>
       <span class="gh-text">{{ typeToLabel[type] }}</span>
     </a>
-    <a class="gh-count" target="_blank" href="{{ count_url }}" [ngStyle]="{ display: showZero || count > 0 ? 'block' : 'none' }">
+    <a class="gh-count" target="_blank" [href]="count_url" [ngStyle]="{ display: showZero || count > 0 ? 'block' : 'none' }">
       {{ count }}
     </a>
     <ng-content></ng-content>
@@ -32,6 +33,8 @@ const isSSR = !(typeof document === 'object' && !!document);
   encapsulation: ViewEncapsulation.Emulated,
   preserveWhitespaces: false,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CommonModule],
 })
 export class GithubButtonComponent implements OnChanges, OnInit, OnDestroy {
   private notify$: Subscription | null = null;
